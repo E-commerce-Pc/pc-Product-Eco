1a3ef5c4879b46498c697b7605827c374574fcf3
shahadkhraisheh96 <shahadkh2004@gmail.com>
2026-05-10 15:35:45 +0300

    all

---
+++ PcProductEco/PcProductEco/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace PcProductEco.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(100, ErrorMessage = "Full Name cannot exceed 100 characters")]
+        public string FullName { get; set; }
+
+        //public ICollection<order>? Orders { get; set; }
+        //public ICollection<Review>? Reviews { get; set; }
+        //public Cart? Cart { get; set; }
+        //public WishList? WishList { get; set; }
+
+    }
+}
